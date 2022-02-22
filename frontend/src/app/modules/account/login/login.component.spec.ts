0b2ac560dee298eded1bea7639f592fac88d8b35
Gilberto Toledo <ogilbertotoledo@gmail.com>
2022-02-22 16:49:44 -0300

    [Frontend] Create unit tests

--- frontend/src/app/modules/account/login/login.component.spec.ts
+++ frontend/src/app/modules/account/login/login.component.spec.ts
@@ -1,4 +1,6 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { LoginComponent } from './login.component';
 
@@ -8,7 +10,8 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      imports: [HttpClientModule,RouterTestingModule],
+      declarations: [ LoginComponent ],
     })
     .compileComponents();
   });
@@ -21,5 +24,14 @@ describe('LoginComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render form', () => {
+    const fixture = TestBed.createComponent(LoginComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('#input_username')).toBeTruthy();
+    expect(compiled.querySelector('#input_password')).toBeTruthy();
+    expect(compiled.querySelector('#btn_login')).toBeTruthy();
   });
 });
--- frontend/src/app/modules/home/checking-account/checking-account.component.spec.ts
+++ frontend/src/app/modules/home/checking-account/checking-account.component.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CheckingAccountComponent } from './checking-account.component';
@@ -8,6 +9,7 @@ describe('CheckingAccountComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientModule],
       declarations: [ CheckingAccountComponent ]
     })
     .compileComponents();
